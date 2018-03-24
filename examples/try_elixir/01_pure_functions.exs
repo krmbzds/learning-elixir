f571712b0e4c636253aba8fc96e7a75ba013f87c
Kerem Bozdas <krmbzds.github@gmail.com>
2018-03-24 21:52:53 +0300

    Add FP vs OO notes

--- examples/try_elixir/01_pure_functions.exs
+++ examples/try_elixir/01_pure_functions.exs
@@ -4,3 +4,21 @@
 #
 # 1. Return value relying entirely on arguments
 # 2. No side effects
+
+# Pure Functions in FP vs. Object State in OO
+#
+# 1. Object State - Object-oriented Languages
+# (Some info is hidden from the caller)
+#
+#   Q: What is my current account balance?
+#   A: It depends on how much you initially had and how much was spent.
+#
+#   account.currentBalance()  # The account object holds the information
+#
+# 2. Pure functions - Functional Languages
+# (Function recievers all data necessary to perform the operation)
+#
+#   Q: Starting the month with $1000, when I spend $200, what is my balance?
+#   A: The account balance is $1000 - $200 = $800
+#
+#   Account.balance(1000, 200)  # No dependencies!
