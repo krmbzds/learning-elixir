44a27a7d47de49ef467a88eca3d124e390001bb1
Kerem Bozdas <krmbzds.github@gmail.com>
2018-03-25 15:22:10 +0300

    Add more examples

--- examples/try_elixir/00_named_functions.exs
+++ examples/try_elixir/00_named_functions.exs
@@ -5,6 +5,10 @@ defmodule Account do
   def balance(initial, spending) do
     initial - spending
   end
+
+  def investment_return(initial, interest) do
+    initial + (initial * interest)
+  end
 end
 
 # Invoking Named Functions
@@ -13,6 +17,8 @@ end
 current_balance = Account.balance(1000, 200)
 IO.puts "Current balance: US $#{current_balance}"
 
+amount = Account.investment_return(1000, 0.0001)
+IO.puts "Investment return: $#{amount}"
+
 # Running Elixir Programs
 elixir 00_named_functions.exs
-
